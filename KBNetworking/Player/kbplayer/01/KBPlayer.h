d6d5f698c81f6646af08b9f101893d11c45b37a4
kobi_2016 <254633397@qq.com>
2016-09-08 19:01:45 +0800

    添加kbplayer

---
+++ KBNetworking/Player/kbplayer/01/KBPlayer.h
@@ -0,0 +1,13 @@
+//
+//  KBPlayer.h
+//  KBNetworking
+//
+//  Created by chengshenggen on 9/8/16.
+//  Copyright © 2016 Gan Tian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KBPlayer : NSObject
+
+@end
---
+++ KBNetworking/Player/kbplayer/01/KBPlayer.m
@@ -0,0 +1,13 @@
+//
+//  KBPlayer.m
+//  KBNetworking
+//
+//  Created by chengshenggen on 9/8/16.
+//  Copyright © 2016 Gan Tian. All rights reserved.
+//
+
+#import "KBPlayer.h"
+
+@implementation KBPlayer
+
+@end
